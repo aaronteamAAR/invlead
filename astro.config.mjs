e5bf29f6fae05df3fe9e0b5d8f3fedf2cd090973
Aaron Chris <rooneychris83@gmail.com>
2023-03-20 15:33:56 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import dasiyui from 'daisyui';
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  integrations: [react(),tailwind({
+    config: {
+      path: './tailwind.config.cjs'
+    },
+    plugins: [dasiyui]
+  }),  sitemap({  
+    i18n: {
+      defaultLocale: 'en', 
+      locales: {
+        en: 'en-US',  
+        es: 'es-ES',
+        fr: 'fr-CA',
+      },
+    },
+  }),]
+});
