37b994e363b8b5484a8fbcaf20f20fab8ae34e6c
Aaron Chris <rooneychris83@gmail.com>
2023-03-30 09:43:24 +0100

    file icon change

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function Card(props : Props) {
     return (
-    <main className="flex flex-col justify-center gap-2 mt-12 mx-auto md:flex flex-row">
+    <main className="flex  justify-center gap-2 mt-12 mx-auto md:flex flex-coljustify-center gap-2 mt-12 mx-auto ">
       <div className="w-[20em] rounded overflow-hidden bg-white shadow-sm md: w-[28em]">
          <img className="w-full" src={props.imgUrl} alt={props.alt}  />
             <div className="px-6 py-4">
---
+++ src/components/MarketPlace.jsx
@@ -0,0 +1,24 @@
+import '../styles/market.css'
+
+
+export default function Market() {
+  return (
+    <div class="basic_block w-[29em] ">
+      <div class="div_content">
+        <ul role="list" class="feat_list">
+          <li>
+            <div class="features_icon"></div>
+          </li>
+          <li class="about_caption">
+            <div class="basic-text">Organization</div>
+          </li>
+          <li class="about_subtitle">
+            <div class="subtitle-small">
+              All slides were named and organized into separate files
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
